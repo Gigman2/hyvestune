0dbabb4e91c445147072e6580cf9b3c6b04fbd4e
Optimus Prime <workwithwhitson@gmail.com>
2021-12-29 08:51:11 +0000

    update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,6 @@
-import { Outlet } from "remix/client";
-import {
-  Box,
-} from '@chakra-ui/react'
+import { Outlet } from 'remix/client';
+import { Box } from '@chakra-ui/react';
 
 export default function Index() {
-  return (
-    <Box>
-      Hello world
-    </Box>
-  );
+  return <Box>Hello world</Box>;
 }
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,99 +1,134 @@
-import { Outlet } from "react-router-dom";
-import {
-  Box, Heading, Button, Text, Icon, Link, Image
-} from '@chakra-ui/react'
-import {AiOutlineGoogle} from 'react-icons/ai'
-import {GrFacebookOption} from 'react-icons/gr'
-import { Input } from "~/components/Forms"
+import { Outlet } from 'react-router-dom';
+import { Box, Heading, Button, Text, Icon, Link, Image } from '@chakra-ui/react';
+import { AiOutlineGoogle } from 'react-icons/ai';
+import { GrFacebookOption } from 'react-icons/gr';
+import { Input } from '~/components/Forms';
 
 export default function Login() {
   return (
-    <Box w={'full'} h={'100vh'} pt="7vh" bg={"url('/images/login-image.png')"} backgroundSize="cover" pos="relative">
-      <Box bg="htGradient.500" w="100%" h="100%" pos={'absolute'} top="0" opacity={0.5} zIndex={2}></Box>
-      <Box w={'700px'} minH="80vh" bg="white" mx="auto" rounded="2xl" p={5} pos={'relative'} zIndex={3}>
-        <Heading textAlign={'center'} fontSize={18} mt={8}>Login</Heading>
-        
-      <Box mt={6} w={"500px"} mx={"auto"}>
-        <Input
-          type="email"
-          borderWidth={2} 
-          borderColor={'htIndigo.500'} 
-          rounded={10} 
-          w="full" 
-          as={Input}
-          required
-          id="email" 
-          label="Email"
-          onBlur={() => null}
-          placeholder=""
-          value={''}
-          error={''}
-          touched={false}
-          onChange={() => null}
-          setFieldTouched={() => null}
-        />
+    <Box
+      w={'full'}
+      h={'100vh'}
+      pt="7vh"
+      bg={"url('/images/login-image.png')"}
+      backgroundSize="cover"
+      pos="relative"
+    >
+      <Box
+        bg="htGradient.500"
+        w="100%"
+        h="100%"
+        pos={'absolute'}
+        top="0"
+        opacity={0.5}
+        zIndex={2}
+      ></Box>
+      <Box
+        w={'700px'}
+        minH="80vh"
+        bg="white"
+        mx="auto"
+        rounded="2xl"
+        p={5}
+        pos={'relative'}
+        zIndex={3}
+      >
+        <Heading textAlign={'center'} fontSize={18} mt={8}>
+          Login
+        </Heading>
 
-        <Input
-          mTop={10}
-          type="password"
-          borderWidth={2} 
-          borderColor={'htIndigo.500'} 
-          rounded={10} 
-          w="full" 
-          as={Input}
-          required
-          id="password" 
-          label="Password"
-          onBlur={() => null}
-          placeholder=""
-          value={''}
-          error={''}
-          touched={false}
-          onChange={() => null}
-          setFieldTouched={() => null}
-        />
-        <Button 
-          bg={'htIndigo.500'} 
-          rounded={10} 
-          w="full" 
-          h={14} 
-          mt={10} 
-          color="white" 
-          fontWeight={500}>Log In</Button>
+        <Box mt={6} w={'500px'} mx={'auto'}>
+          <Input
+            type="email"
+            borderWidth={2}
+            borderColor={'htIndigo.500'}
+            rounded={10}
+            w="full"
+            as={Input}
+            required
+            id="email"
+            label="Email"
+            onBlur={() => null}
+            placeholder=""
+            value={''}
+            error={''}
+            touched={false}
+            onChange={() => null}
+            setFieldTouched={() => null}
+          />
 
-        <Text textAlign={"center"} mt={4}>or</Text>
-        <Button 
-          bg={'rgba(222, 82, 70, 1)'} 
-          _hover={{
-            bg: '#bf3e32'
-          }}
-          rounded={10} 
-          w="full" 
-          h={14} 
-          mt={4} 
-          color="white" 
-          fontWeight={500}>
+          <Input
+            mTop={10}
+            type="password"
+            borderWidth={2}
+            borderColor={'htIndigo.500'}
+            rounded={10}
+            w="full"
+            as={Input}
+            required
+            id="password"
+            label="Password"
+            onBlur={() => null}
+            placeholder=""
+            value={''}
+            error={''}
+            touched={false}
+            onChange={() => null}
+            setFieldTouched={() => null}
+          />
+          <Button
+            bg={'htIndigo.500'}
+            rounded={10}
+            w="full"
+            h={14}
+            mt={10}
+            color="white"
+            fontWeight={500}
+          >
+            Log In
+          </Button>
+
+          <Text textAlign={'center'} mt={4}>
+            or
+          </Text>
+          <Button
+            bg={'rgba(222, 82, 70, 1)'}
+            _hover={{
+              bg: '#bf3e32',
+            }}
+            rounded={10}
+            w="full"
+            h={14}
+            mt={4}
+            color="white"
+            fontWeight={500}
+          >
             <Icon as={AiOutlineGoogle} mr={2} boxSize={6} /> Continue with google
-        </Button>
-        <Button 
-          bg={'rgba(66, 103, 178, 1)'} 
-          _hover={
-            {
-              bg:'#2f5299'
-            }
-          }
-          rounded={10} 
-          w="full" 
-          h={14} 
-          mt={6} 
-          color="white" 
-          fontWeight={500}>
-          <Icon as={GrFacebookOption} mr={1} boxSize={6} />Continue with facebook
-        </Button>
+          </Button>
+          <Button
+            bg={'rgba(66, 103, 178, 1)'}
+            _hover={{
+              bg: '#2f5299',
+            }}
+            rounded={10}
+            w="full"
+            h={14}
+            mt={6}
+            color="white"
+            fontWeight={500}
+          >
+            <Icon as={GrFacebookOption} mr={1} boxSize={6} />
+            Continue with facebook
+          </Button>
           <Outlet />
-        <Text textAlign={"center"} mt={4} mb={8}>I do not have an account? <Link to={{pathname: '/'}} color="blue.400">register here</Link></Text>
+          <Text textAlign={'center'} mt={4} mb={8}>
+            I do not have an account?{' '}
+            <Link to={{ pathname: '/' }} color="blue.400">
+              register here
+            </Link>
+          </Text>
+        </Box>
       </Box>
-    </Box>
     </Box>
   );
 }
