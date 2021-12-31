1b74aed7c271f9ffa7e786b04577b43771c6e4c6
Optimus Prime <workwithwhitson@gmail.com>
2021-12-31 06:19:59 +0000

    update

---
+++ app/routes/wallet.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import Layout from '~/containers/layout/Layout';
+
+const Wallet = () => {
+  return (
+    <Layout page={3} showHeader>
+    
+    </Layout>
+  );
+};
+
+export default Wallet;
